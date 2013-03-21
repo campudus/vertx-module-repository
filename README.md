e93f3104c403056ebb9e964bcf7f3cf6be25b735
Jaroslav Bobrowski <bj@campudus.com>
2013-03-21 04:55:04 -0700

    Initial gradle template for vert.x 2.0

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-vertx-module-repository
-=======================
+# Vert.x Gradle Template
 
-The Vert.x module registry
+Template project for creating a Vert.x module with a Gradle build.
+
+Clone this and adapt it to easily develop Vert.x modules using Gradle as your build tool.
+
+See the [build script](build.gradle) for the list of useful tasks
---
+++ src/test/resources/integration_tests/javascript/mytest.js
@@ -0,0 +1,20 @@
+load("vertx.js");
+load("vertx_tests.js");
+
+// The test methods must begin with "test"
+
+function test_1() {
+  vassert.testComplete()
+}
+
+function test_2() {
+  vassert.assertEquals("foo", "foo")
+  vassert.testComplete()
+}
+
+var script = this;
+vertx.deployModule(java.lang.System.getProperty("vertx.modulename"), null, 1, function(depID) {
+  initTests(script);
+});
+
+
