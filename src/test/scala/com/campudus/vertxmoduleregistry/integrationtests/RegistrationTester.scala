62e7280f4f881b2ee72ae1748aafb6d03f9a3a46
Jaroslav Bobrowski <bj@campudus.com>
2013-04-29 09:40:38 +0200

    fixed: Handler with AsyncResult

--- src/test/scala/com/campudus/vertxmoduleregistry/integrationtests/RegistrationTester.scala
+++ src/test/scala/com/campudus/vertxmoduleregistry/integrationtests/RegistrationTester.scala
@@ -4,13 +4,14 @@ import org.junit.Test
 import org.vertx.java.core.Handler
 import org.vertx.testtools.VertxAssert._
 import org.vertx.testtools.TestVerticle
+import org.vertx.java.core.AsyncResult
 
 class RegistrationTester extends TestVerticle {
 
   @Test
   def testDeployMod() {
-    container.deployModule(System.getProperty("vertx.modulename"), new Handler[String]() {
-      override def handle(deploymentID: String) {
+    container.deployModule(System.getProperty("vertx.modulename"), new Handler[AsyncResult[String]]() {
+      override def handle(deploymentID: AsyncResult[String]) {
         assertNotNull("deploymentID should not be null", deploymentID)
         testComplete()
       }
