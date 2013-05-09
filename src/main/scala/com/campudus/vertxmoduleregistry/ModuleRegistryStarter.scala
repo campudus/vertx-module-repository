4110b53c2762f2648c3966023d5aecf49caf60e4
Joern Bernhardt <joern.bernhardt@googlemail.com>
2013-05-09 13:45:48 +0200

    deploying on the right context

--- src/main/scala/com/campudus/vertxmoduleregistry/ModuleRegistryStarter.scala
+++ src/main/scala/com/campudus/vertxmoduleregistry/ModuleRegistryStarter.scala
@@ -1,15 +1,14 @@
 package com.campudus.vertxmoduleregistry
 
-import scala.concurrent.{Future, Promise}
-import scala.concurrent.ExecutionContext.Implicits.global
-
+import scala.concurrent.{ Future, Promise }
 import org.vertx.java.core.AsyncResult
 import org.vertx.java.core.json.JsonObject
 import org.vertx.java.platform.Verticle
-
 import com.campudus.vertx.helpers.VertxScalaHelpers
 import com.campudus.vertxmoduleregistry.database.Database
 import com.campudus.vertxmoduleregistry.security.Authentication
+import org.vertx.java.core.AsyncResultHandler
+import org.vertx.java.core.Handler
 
 class ModuleRegistryStarter extends Verticle with VertxScalaHelpers {
 
@@ -17,56 +16,83 @@ class ModuleRegistryStarter extends Verticle with VertxScalaHelpers {
   val authManagerModName = "io.vertx~mod-auth-mgr~2.0.0-SNAPSHOT"
   val unzipModName = "io.vertx~mod-unzip~1.0.0-SNAPSHOT"
   val unzipAddress = "io.vertx.unzipper"
+  val serverVerticle = "com.campudus.vertxmoduleregistry.ModuleRegistryServer"
 
   override def start() {
+    implicit val ec = scala.concurrent.ExecutionContext.global
     lazy val logger = container.logger()
 
     logger.error("Starting module registry ...")
 
-    val config = Option(container.config()).getOrElse(json)
-    val configDb = config.getObject("database", json)
+    val config = Option(container.config()).getOrElse(new JsonObject)
+    val configDb = config.getObject("database", new JsonObject)
       .putString("address", Database.dbAddress)
-    val configAuth = config.getObject("auth", json)
+    val configAuth = config.getObject("auth", new JsonObject)
       .putString("persistor_address", Database.dbAddress)
       .putString("address", Authentication.authAddress)
-    val configUnzip = config.getObject("unzip", json)
+    val configUnzip = config.getObject("unzip", new JsonObject)
       .putString("address", unzipAddress)
 
-    def handleDeployError(onSuccess: String => Unit) = { deployResult: AsyncResult[String] =>
-      if (deployResult.succeeded()) {
-        onSuccess(deployResult.result())
-      } else {
-        logger.error("failed to start verticle " + deployResult.cause())
-        deployResult.cause().printStackTrace()
+    println("deploying all modules with " + config)
+
+    val ctx = vertx.currentContext()
+
+    deployModule(mongoPersistorModName, configDb)
+      .map(id => println("deployed mongo persistor with id: " + id))
+      .flatMap(_ => deployModule(authManagerModName, configAuth))
+      .map(id => println("deployed auth manager with id: " + id))
+      .flatMap(_ => deployModule(unzipModName, configUnzip))
+      .map(id => println("deployed unzip module with id: " + id))
+      .map { _ =>
+        println("modules should be deployed now, deploying verticle")
+        ctx.runOnContext({ () =>
+          container.deployVerticle(serverVerticle, config, { deployResult: AsyncResult[String] =>
+            if (deployResult.succeeded()) {
+              println("started " + serverVerticle + " with config " + config)
+            } else {
+              println("failed to start " + serverVerticle + " because of " + deployResult.cause())
+              deployResult.cause().printStackTrace()
+            }
+          })
+        })
       }
-    }
 
-    val future = (deployModule(mongoPersistorModName, configDb)
-      .map(_ => deployModule(mongoPersistorModName, configAuth))
-      .map(_ => deployModule(unzipModName, configUnzip)))
+    println("Modules should deploy async now.")
 
-    logger.error("deploying ModuleRegistryServer with " + config)
-    container.deployVerticle("com.campudus.vertxmoduleregistry.ModuleRegistryServer", config,
-      handleDeployError { deploymentId: String =>
-        logger.error("Module registry started")
-      })
-
-    logger.info("Modules should deploy async now.")
   }
 
   override def stop() {
     println("Module registry stopped.")
   }
 
+  private def deployVerticle(name: String, config: JsonObject): Future[String] = {
+    val p = Promise[String]
+    container.deployVerticle(name, config, new AsyncResultHandler[String]() {
+      def handle(deployResult: AsyncResult[String]) = {
+        if (deployResult.succeeded()) {
+          println("started " + name + " with config " + config)
+          p.success(deployResult.result())
+        } else {
+          println("failed to start " + name + " because of " + deployResult.cause())
+          deployResult.cause().printStackTrace()
+          p.failure(deployResult.cause())
+        }
+      }
+    })
+    p.future
+  }
+
   private def deployModule(name: String, config: JsonObject): Future[String] = {
     val p = Promise[String]
-    container.deployModule(name, config, { deployResult: AsyncResult[String] =>
-      if (deployResult.succeeded()) {
-        println("started " + name + " with config " + config)
-        p.success(deployResult.result())
-      } else {
-        println("failed to start " + name + " because of " + deployResult.cause())
-        p.failure(deployResult.cause())
+    container.deployModule(name, config, new AsyncResultHandler[String]() {
+      def handle(deployResult: AsyncResult[String]) = {
+        if (deployResult.succeeded()) {
+          println("started " + name + " with config " + config)
+          p.success(deployResult.result())
+        } else {
+          println("failed to start " + name + " because of " + deployResult.cause())
+          p.failure(deployResult.cause())
+        }
       }
     })
     p.future
