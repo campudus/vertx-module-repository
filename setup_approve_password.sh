7140dd6716741bdfc3cbeb86ea4f1b38ef16d801
Joern Bernhardt <joern.bernhardt@googlemail.com>
2013-05-13 09:06:46 +0200

    setup script for moderator password

---
+++ setup_approve_password.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ "" == $1 ]]; then
+  echo 'You need to provide a password for the moderator. Please run'
+  echo '    ./setup.sh <your_password_here>'
+
+  return 1
+fi
+
+source .openshift/vertx-module-registry.config
+
+mongo ${OPENSHIFT_MONGODB_DB_HOST}:${OPENSHIFT_MONGODB_DB_PORT}/${MONGO_DB_NAME} --eval "use ${MONGO_DB_NAME};db.users.update({username:'approver'},{username:'approver',password:'${1}'},{upsert:true});"
